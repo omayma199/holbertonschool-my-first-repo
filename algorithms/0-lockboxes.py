f8bf28c4ef5f91631140bc75e1e4c36d4206150e
omayma19 <5752@holbertonstudents.com>
2025-07-23 18:30:36 +0100

    test

--- algorithms/0-lockboxes.py
+++ algorithms/0-lockboxes.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
+"""
+lockboxes , we have n number  of locked boxes in front of you.
+ Each box is numbered sequentially from 0 to
+ n - 1 and each box may contain keys to the other boxes.
+"""
+
+
 def canUnlockAll(boxes):
-
-    if (len(boxes) == 0):
+    '''determines if all the boxes can be opned'''
+    open = [0]
+    for i in open:
+        for box in boxes[i]:
+            if box not in open:
+                if box < len(boxes):
+                    open.append(box)
+    if len(open) == len(boxes):
         return True
-
-    keys = set()
-    new_keys = set(boxes[0])
-
-    # while new keys can still be found
-    while (len(new_keys)):
-
-        # add new keys to ring
-        keys = keys | new_keys
-        new_keys = set()
-
-        # check keyring for intersection with solution: set { 1..<last box #> }
-        if (keys & set(range(1, len(boxes))) == set(range(1, len(boxes)))):
-            return True
-
-        # use current keyring to get a new round of keys
-        for x in keys:
-            # index protection for keys that have no boxes
-            if (x < len(boxes)):
-                # open box, add keys
-                for y in boxes[x]:
-                    if y not in keys:
-                        new_keys.add(y)
-
     return False
