98bcda74f39a5f5c1e2c4a6e10a381c813630fd9
omayma19 <5752@holbertonstudents.com>
2025-07-24 15:14:38 +0100

    test

--- algorithms/0-is_palindrome.c
+++ algorithms/0-is_palindrome.c
@@ -1,38 +1,49 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include "lists.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to a pointer to the first node
- * Return: 1 if palindrome, 0 otherwise
+ * check_recursively - Recursively moves the pointers to check if palindrome
+ * @start: Will move from the start of the list
+ * @end: Will move from the end of the list
+ *
+ * Return: Either 0 or 1 depending if is a palindrome
+ */
+int check_recursively(listint_t **start, listint_t *end)
+{
+	int check;
+
+	if (end == NULL)
+		return (1);
+
+	check = check_recursively(start, end->next);
+
+	if (check == 0)
+		return (0);
+
+	if ((*start)->n == end->n)
+		check = 1;
+	else
+		check = 0;
+
+	*start = (*start)->next;
+
+	return (check);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: The start of the linked list
+ *
+ * Return: 1 if is palindrome, 0 if it is not
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head;
-    int buffer[10240]; /* stack */
-    int top = 0, i;
+	listint_t **start = head;
+	listint_t *end = *head;
 
-    if (!head || !*head)
-        return (1);
+	if (*head == NULL)
+		return (1);
 
-    while (fast && fast->next)
-    {
-        buffer[top++] = slow->n;
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    if (fast)
-        slow = slow->next;
-
-    i = top - 1;
-    while (slow)
-    {
-        if (slow->n != buffer[i--])
-            return (0);
-        slow = slow->next;
-    }
-
-    return (1);
+	return (check_recursively(start, end));
 }
