50543dad0ec216dd804e92046e7b258ca8432d56
omayma19 <5752@holbertonstudents.com>
2025-08-26 15:50:38 +0100

    test

---
+++ cyber-assessment/6-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# This script takes a URL, sends a request and prints the X-Request-Id header
+import sys
+from urllib import request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
